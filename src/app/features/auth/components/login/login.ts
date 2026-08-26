be4cdeeeb616e62471c445c5d513bc99c325305c
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-26 11:59:01 +0200

    feat(auth): Implement login form with validation and error handling

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { faSolidEnvelope, faSolidLock } from '@ng-icons/font-awesome/solid';
+import { LoginRequest } from '../../models/login-request';
+import { debounce, email, form, FormField, required } from '@angular/forms/signals';
 
 @Component({
-  imports: [NgIcon],
-  providers: [provideIcons({faSolidEnvelope, faSolidLock})],
+  imports: [NgIcon, FormField],
+  providers: [provideIcons({ faSolidEnvelope, faSolidLock })],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  loginModel = signal<LoginRequest>({ email: '', password: '' });
+
+  loginForm = form(this.loginModel, (schemaPath) => {
+    debounce(schemaPath.email, 100);
+    debounce(schemaPath.password, 100);
+    required(schemaPath.email, { message: 'Email adresa je obavezna' });
+    email(schemaPath.email, { message: 'Email adresa nije validna' });
+    required(schemaPath.password, { message: 'Lozinka je obavezna' });
+  });
+}
