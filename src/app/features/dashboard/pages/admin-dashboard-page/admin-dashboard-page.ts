a829a0cd832fca4b857a21b4b8403ac6548fe612
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-05 10:22:34 +0200

    feat(dashboard): Remove dashboard page

--- src/app/features/dashboard/pages/admin-dashboard-page/admin-dashboard-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-admin-dashboard-page',
-  styleUrl: './admin-dashboard-page.css',
-  templateUrl: './admin-dashboard-page.html',
-})
-export class AdminDashboardPage {}
