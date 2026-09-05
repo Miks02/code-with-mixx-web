b40fce633093502720e26484d1bbf5b07ef1e21d
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-05 17:12:04 +0200

    feat(analytics): Add UpcomingClass and UpcomingProject type definitions

---
+++ src/app/features/analytics/models/upcoming-class-.ts
@@ -0,0 +1,8 @@
+export type UpcomingClass = {
+  id: number,
+  subjectName: string,
+  studentName: string,
+  startsAt: string,
+  endsAt: string,
+  
+}
---
+++ src/app/features/analytics/models/upcoming-project.ts
@@ -0,0 +1,7 @@
+export type UpcomingProject = {
+  id: number;
+  subjectName: string;
+  studentName: string;
+  startDate: string;
+  endDate: string;
+}
