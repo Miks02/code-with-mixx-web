31f64b8440b4921bc4f54aec5d020daaf3af71f3
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-05 07:13:47 +0200

    feat(dashboard): Add admin dashboard and layout components with initial structure

---
+++ src/app/features/dashboard/pages/admin-dashboard-page/admin-dashboard-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-admin-dashboard-page',
+  styleUrl: './admin-dashboard-page.css',
+  templateUrl: './admin-dashboard-page.html',
+})
+export class AdminDashboardPage {}
