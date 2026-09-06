1a59619657f577bd379bb8b0aad69a0d99dfa251
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-06 12:11:18 +0200

    chore(deps): Provide tanstack query in app.config as dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTanStackQuery(new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 5 * 60 * 1000,
+          retry: (failureCount, error: any) => {
+            const errorStatus = error.status;
+            if (errorStatus >= 400 && errorStatus < 500)
+              return false;
+            return failureCount < 3;
+          },
+        },
+      },
+    })),
   ]
 };
