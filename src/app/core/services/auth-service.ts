94f346349ad2c39c0456e3f0476e19252a1f663c
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-12 13:28:20 +0200

    feat(auth): Add logout functionality

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Service, Signal, WritableSignal, computed, effect, inject, signal } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { LoginRequest } from '../../features/auth/models/login-request';
+import { computed, inject, Service, signal, WritableSignal } from '@angular/core';
 import { injectMutation, injectQuery } from '@tanstack/angular-query-experimental';
 import { lastValueFrom, Observable, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { LoginRequest } from '../../features/auth/models/login-request';
 import { ProblemDetails } from '../models/problem-details';
 import { UserDetails } from '../models/user-details';
 
@@ -20,14 +20,22 @@ export class AuthService {
   isAdmin = computed(() => this._userDetails()?.roles.includes('Admin') ?? false);
 
   loginMutation = injectMutation<UserDetails, ProblemDetails, LoginRequest>(() => ({
-    mutationFn: async (request: LoginRequest) => lastValueFrom(this.login(request)),
+    mutationFn: (request: LoginRequest) => lastValueFrom(this.login(request)),
     onSuccess: (data: UserDetails) => this._userDetails.set(data),
+  }));
+
+  logoutMutation = injectMutation<void, void, void>(() => ({
+    mutationFn: () => lastValueFrom(this.logout()),
+    onSettled: () => {
+      this._userDetails.set(null);
+      window.location.href = '/login';
+    },
   }));
 
   getMeQuery = injectQuery<UserDetails, void>(() => ({
     queryKey: ['me'],
-    queryFn: async () =>
-      await lastValueFrom(
+    queryFn: () =>
+      lastValueFrom(
         this.http.get<UserDetails>(`${this.apiUrl}/auth/me`, { withCredentials: true }).pipe(
           tap((res) => {
             this._userDetails.set(res);
@@ -41,5 +49,15 @@ export class AuthService {
     return this.http.post<UserDetails>(`${this.apiUrl}/auth/login`, request, {
       withCredentials: true,
     });
+  }
+
+  private logout(): Observable<void> {
+    return this.http.post<void>(
+      `${this.apiUrl}/auth/logout`,
+      {},
+      {
+        withCredentials: true,
+      },
+    );
   }
 }
