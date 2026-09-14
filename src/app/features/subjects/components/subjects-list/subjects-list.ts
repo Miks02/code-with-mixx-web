c10f00906ddfb7a08a13cf495fa568d47fab135c
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-14 22:40:30 +0200

    feat(subjects): Create subject-list and subject-card components

---
+++ src/app/features/subjects/components/subjects-list/subjects-list.ts
@@ -0,0 +1,57 @@
+import { Component, computed, input, output } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  faSolidChevronLeft,
+  faSolidChevronRight,
+  faSolidMagnifyingGlass,
+} from '@ng-icons/font-awesome/solid';
+import { PagedResult } from '../../../../core/models/paged-result';
+import { Button } from '../../../../shared/button/button';
+import { SubjectItem } from '../../models/subject-item';
+import { SubjectCard } from '../subject-card/subject-card';
+
+@Component({
+  imports: [SubjectCard, NgIcon, Button],
+  providers: [
+    provideIcons({ faSolidChevronLeft, faSolidChevronRight, faSolidMagnifyingGlass }),
+  ],
+  selector: 'app-subjects-list',
+  styleUrl: './subjects-list.css',
+  templateUrl: './subjects-list.html',
+})
+export class SubjectsList {
+  pagedSubjects = input.required<PagedResult<SubjectItem> | undefined>();
+  selectedSubjectId = input<number | undefined>(undefined);
+
+  subjectSelected = output<SubjectItem>();
+  searchChanged = output<string>();
+  pageChanged = output<number>();
+
+  subjects = computed(() => this.pagedSubjects()?.items);
+  totalCount = computed(() => this.pagedSubjects()?.totalCount ?? 0);
+  pageSize = computed(() => this.pagedSubjects()?.pageSize ?? 0);
+  paginatedCount = computed(() => this.pagedSubjects()?.paginatedCount ?? 0);
+  currentPage = computed(() => this.pagedSubjects()?.pageNumber ?? 1);
+  totalPages = computed(() => this.pagedSubjects()?.totalPages ?? 1);
+  hasPreviousPage = computed(() => this.pagedSubjects()?.hasPreviousPage ?? false);
+  hasNextPage = computed(() => this.pagedSubjects()?.hasNextPage ?? false);
+
+  onSubjectSelected(subject: SubjectItem) {
+    this.subjectSelected.emit(subject);
+  }
+
+  onSearchInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.searchChanged.emit(target.value);
+  }
+
+  onPreviousPage() {
+    if (!this.hasPreviousPage()) return;
+    this.pageChanged.emit(this.currentPage() - 1);
+  }
+
+  onNextPage() {
+    if (!this.hasNextPage()) return;
+    this.pageChanged.emit(this.currentPage() + 1);
+  }
+}
