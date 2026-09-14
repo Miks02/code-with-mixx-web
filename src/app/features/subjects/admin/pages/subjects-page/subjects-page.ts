c4fb706c73135c752d592badac698f683ef7dad3
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-14 22:45:40 +0200

    feat(subjects): Orchestrate all previously created subject components via the subjects-page with real data

--- src/app/features/subjects/admin/pages/subjects-page/subjects-page.ts
+++ src/app/features/subjects/admin/pages/subjects-page/subjects-page.ts
@@ -1,9 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, signal, WritableSignal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { provideIcons } from '@ng-icons/core';
+import {
+  faSolidBook,
+  faSolidBookOpen,
+  faSolidBookSkull,
+  faSolidCalendar,
+  faSolidUserGraduate,
+} from '@ng-icons/font-awesome/solid';
+import { BehaviorSubject, debounceTime } from 'rxjs';
+import { SubjectsList } from '../../../components/subjects-list/subjects-list';
+import { SubjectItem } from '../../../models/subject-item';
+import { SubjectService } from '../../../services/subject-service';
+import { CreateSubjectForm } from '../../components/create-subject-form/create-subject-form';
+import { MostPopularSubject } from '../../components/most-popular-subject/most-popular-subject';
+import { SubjectDetails } from '../../components/subject-details/subject-details';
+import { StatsCard } from '../../../components/stats-card/stats-card';
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule,
+    SubjectsList,
+    SubjectDetails,
+    MostPopularSubject,
+    CreateSubjectForm,
+    StatsCard,
+  ],
+  providers: [
+    provideIcons({
+      faSolidCalendar,
+      faSolidUserGraduate,
+      faSolidBook,
+      faSolidBookSkull,
+      faSolidBookOpen,
+    }),
+  ],
   selector: 'app-subjects-page',
   styleUrl: './subjects-page.css',
   templateUrl: './subjects-page.html',
 })
-export class SubjectsPage {}
+export class SubjectsPage {
+  private subjectService = inject(SubjectService);
+  private searchTerm$ = new BehaviorSubject<string>('');
+  private searchTerm = toSignal(this.searchTerm$.pipe(debounceTime(300)), { initialValue: '' });
+
+  private pageNumber: WritableSignal<number> = signal(1);
+  private pageSize: WritableSignal<number> = signal(15);
+
+  subjectsSummarySource = this.subjectService.getSubjectsSummaryForAdminQuery(
+    this.pageNumber,
+    this.pageSize,
+    this.searchTerm,
+  );
+
+  subjectsSource = this.subjectService.getPagedSubjectsForAdminQuery(
+    this.subjectsSummarySource,
+    this.pageNumber,
+    this.pageSize,
+    this.searchTerm,
+  );
+
+  subjectsSummary = computed(() => this.subjectsSummarySource.data());
+  subjects = computed(() => this.subjectsSource?.data());
+  selectedSubject: WritableSignal<SubjectItem | undefined> = signal(undefined);
+
+  mostPopularSubject = computed(() => this.subjectsSummary()?.mostPopularSubject);
+
+  onSearchChange(searchTerm: string) {
+    this.pageNumber.set(1);
+    this.searchTerm$.next(searchTerm);
+  }
+
+  onPageChanged(page: number) {
+    this.pageNumber.set(page);
+  }
+
+  onSubjectSelected(subject: SubjectItem) {
+    this.selectedSubject.set(subject);
+  }
+}
