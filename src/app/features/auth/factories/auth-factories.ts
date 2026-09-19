84bb06663969cc605a5c76ffd09fc208f54709c6
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-19 21:59:02 +0200

    feat(auth): Implement factory function for creating a form for forgotten password

--- src/app/features/auth/factories/auth-factories.ts
+++ src/app/features/auth/factories/auth-factories.ts
@@ -1,5 +1,6 @@
 import { debounce, email, form, required, validate } from '@angular/forms/signals';
 import { LoginRequest } from '../models/login-request';
+import { ForgotPasswordRequest } from '../models/forgot-password-request';
 import { Signal, WritableSignal } from '@angular/core';
 import { ProblemDetails } from '../../../core/models/problem-details';
 
@@ -12,5 +13,13 @@ export function createLoginForm(
     required(schemaPath.email, { message: 'Email adresa je obavezna' });
     email(schemaPath.email, { message: 'Email adresa nije validna' });
     required(schemaPath.password, { message: 'Lozinka je obavezna' });
+  });
+}
+
+export function createForgotPasswordForm(request: WritableSignal<ForgotPasswordRequest>) {
+  return form(request, (schemaPath) => {
+    debounce(schemaPath.email, 100);
+    required(schemaPath.email, { message: 'Email adresa je obavezna' });
+    email(schemaPath.email, { message: 'Email adresa nije validna' });
   });
 }
