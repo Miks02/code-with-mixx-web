29ef0bc3bda7ea12d7f216f9e4073f1049f40d57
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-20 06:12:06 +0200

    feat(forgot-password): Modify the component so that it incorporates recent changes in auth service

--- src/app/features/auth/components/forgot-password/forgot-password.ts
+++ src/app/features/auth/components/forgot-password/forgot-password.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormField, submit } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
@@ -6,6 +6,8 @@ import { faSolidEnvelope, faSolidPaperPlane } from '@ng-icons/font-awesome/solid
 import { Button } from '../../../../shared/button/button';
 import { createForgotPasswordForm } from '../../factories/auth-factories';
 import { ForgotPasswordRequest } from '../../models/forgot-password-request';
+import { AuthService } from '../../../../core/services/auth-service';
+import { ToastService } from '../../../../core/services/toast-service';
 
 @Component({
   imports: [NgIcon, FormField, Button, RouterLink],
@@ -15,8 +17,9 @@ import { ForgotPasswordRequest } from '../../models/forgot-password-request';
   templateUrl: './forgot-password.html',
 })
 export class ForgotPassword {
-  isOnCooldown = computed(() => this.cooldownDuration() > 0);
-  cooldownDuration = signal(0);
+  private authService = inject(AuthService);
+  private toastService = inject(ToastService);
+  
   messageWrapper = signal({
     title: '',
     text: '',
@@ -25,24 +28,31 @@ export class ForgotPassword {
   forgotPasswordModel = signal<ForgotPasswordRequest>({ email: '' });
   forgotPasswordForm = createForgotPasswordForm(this.forgotPasswordModel);
 
+  isOnCooldown = this.authService.isOnCooldown
+  cooldownDuration = this.authService.cooldownDuration;
+  getResetPasswordTokenMutation = this.authService.getResetPasswordTokenMutation;
+  isPending = this.getResetPasswordTokenMutation.isPending;
+
   onSubmit() {
     return submit(this.forgotPasswordForm, async () => {
-      this.createMessageWrapper(
-        'Zahtev je uspešno poslat!',
-        'Ako postoji nalog s tim emailom, biće ti poslat email za resetovanje lozinke.',
-      );
-      this.beginCooldown();
+      try {
+        await this.getResetPasswordTokenMutation.mutateAsync(this.forgotPasswordModel());
+
+        this.createMessageWrapper(
+          'Zahtev je uspešno poslat!',
+          'Ako postoji nalog s tim emailom, biće ti poslat email za resetovanje lozinke.',
+        );
+      } catch (err: any) {
+        if (err?.status === 429) {
+          this.toastService.showError('Previše zahteva. Pokušaj ponovo kasnije.', 'Greška 429');
+          return [];
+        }
+
+        this.toastService.showError('Došlo je do neočekivane greške. Pokušaj ponovo kasnije.');
+      }
+
       return [];
     });
-  }
-
-  beginCooldown() {
-    this.cooldownDuration.set(2);
-
-    let interval = setInterval(() => {
-      this.cooldownDuration.update((duration) => duration - 1);
-      if (this.cooldownDuration() === 0) clearInterval(interval);
-    }, 1000);
   }
 
   private createMessageWrapper(title: string, text: string) {
